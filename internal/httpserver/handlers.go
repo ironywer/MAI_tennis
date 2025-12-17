fe8bf569ff29a2878a93436d1c06d10cf2fa0f80
ironywer <leva.vtorov@yandex.ru>
2025-12-17 23:07:53 +0300

    fix link

--- internal/httpserver/handlers.go
+++ internal/httpserver/handlers.go
@@ -9,7 +9,7 @@ type Handlers struct{}
 func (h *Handlers) Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("<html><body><link rel=\"stylesheet\" href=\"/static/css/style.css\"><h1>Welcome to the Index Page</h1></body></html>"))
+	w.Write([]byte("<html><head><link rel=\"stylesheet\" href=\"/static/css/style.css\"></head><body><h1>Welcome to the Index Page</h1></body></html>"))
 }
 func (h *Handlers) Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
